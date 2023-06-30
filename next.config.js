ef6d2c2e202731e5f2ec574286b8174d59c90fca
Newton Duarte <newton_@hotmail.com>
2023-06-30 05:47:45 -0300

    chore: add modularize imports next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  modularizeImports: {
+    '@acme/ui': {
+      transform: '@acme/ui/dist/{{member}}',
+    },
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    },
+  },
 }
 
 module.exports = nextConfig
